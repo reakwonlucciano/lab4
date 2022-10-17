83867db8933c36886e68cb162676610ce5b8152e
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-10-17 14:11:43 +0100

    Update create.js

--- src/components/create.js
+++ src/components/create.js
@@ -1,12 +1,17 @@
+//import react
 import React from "react";
 
+// class for the create component// extends and export word used to export app.js
 export class Create extends React.Component{
 
     render(){
+        
+        //returning componets  
+        
         return(
             <div>
                 <h3>Hello from my create component!</h3>
             </div>
         );
-    }
-}
+    } //return 
+} //class
